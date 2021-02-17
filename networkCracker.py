7930100d1189519b62be862eb13f672d49037b8d
cantsnn <cantosun28@yandex.com>
2021-02-17 17:26:59 +0300

    bssid channel numaralari cekildi

--- networkCracker.py
+++ networkCracker.py
@@ -2,9 +2,18 @@
 import os 
 import subprocess   
 from subprocess import check_call
+from scapy.all import *
+import sys, signal 
+from multiprocessing import Process
+import time
+import csv
+import pandas as pd
 
 if os.path.exists('output.txt'):
   os.remove('output.txt')
+
+if os.path.exists('myOutput-01.csv'):
+  os.remove('myOutput-01.csv')
 #interface adını 9-17. satırlarda alıyoruz
 os.system('airmon-ng >> output.txt')
 f = open('output.txt', 'r')
@@ -26,15 +35,26 @@ for line in f:
     if i == 6:
       interfacemon = word
       print(interfacemon)
-order = f"airodump-ng {interfacemon} -M >> scan.txt & sleep 2; kill $!"
+
+order = f"airodump-ng {interfacemon} -M -w myOutput --output-format csv & sleep 20; kill $!"
 geny = os.system(order)
-#geny  = os.system("airodump-ng wlan0mon -M & sleep 20; kill $!")
-#with open("scan.txt", "w", encoding="utf-8") as filetxt:
-  #filetxt.write(geny)
-#cmd = os.system("sleep 15")
-#os.system('{} >> scan.txt'.format(order))
-
-
-
-
-
+print("****************1***************************")
+in_file = pd.read_csv("input.csv")
+inputCSV = in_file.copy()
+print("****************5***************************")
+in_file1= pd.read_csv("myOutput-01.csv",sep=r'\s*,\s*',header=0, engine='python')
+outputCSV= in_file1.copy()
+print("****************6***************************")
+inBSSID = list(inputCSV["BSSID"])
+outBSSID = list(outputCSV["BSSID"])
+print("******************2*************************")
+print("list len ",len(outBSSID))
+print("*******************3************************")
+for item in outBSSID:
+    if item in inBSSID:
+        filter = outputCSV["BSSID"]==item
+        ch = int(outputCSV[filter]["channel"])
+        print(item + ": ",ch)
+        print("***************4****************************")
+    else: 
+      print(item," : bulunamadi")
